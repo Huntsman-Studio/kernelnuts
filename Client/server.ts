fa2b8c3abe68a8c308b977ec36c291cb4a7afc4a
klamaj <kri.lamaj@gmail.com>
2022-08-15 10:49:15 +0300

    Publish v.1.0

--- Client/server.ts
+++ Client/server.ts
@@ -11,8 +11,8 @@ import { existsSync } from 'fs';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/Client/browser');
-  // const distFolder = join(process.cwd(), '../browser'); // prod
+  // const distFolder = join(process.cwd(), 'dist/Client/browser');
+  const distFolder = join(process.cwd(), '../browser'); // prod
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
